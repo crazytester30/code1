ea2c032291e09d5b120faa7b271768f09b2d3c26
unknown <pentest3010@outlook.com>
2021-11-18 06:14:36 +0000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace code1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
